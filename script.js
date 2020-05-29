6f3044207d4bf38212d9294bb887af7ee9b65854
ElisangelaG <elisangelacristinne@gmail.com>
2020-05-28 21:57:33 -0300

    Exercicio 2 Finalizado

---
+++ script.js
@@ -0,0 +1,39 @@
+var receita = {
+    titulo: "Chocolate",
+    porcoes: 2,
+    ingredientes: ["cacau", "leite", "manteiga"]
+
+}
+
+for (i = 0; i < receita.ingredientes.length; i++) {
+    console.log(ingredientes[i])
+}
+
+let livros = [{
+
+        titulo: "O menino do pijama listrado",
+        autor: "John Boyne",
+        livrolido: true,
+    },
+    {
+        titulo: "Como eu era antes de vc",
+        autor: "Jojo Moyes",
+        livrolido: true,
+    },
+
+    {
+        titulo: "Como fiquei depois de vc",
+        autor: "Jojo Moyes",
+        livrolido: true,
+    },
+
+    {
+        titulo: "Como eu era antes de vc",
+        autor: "Jojo Moyes",
+        livrolido: true,
+    }
+
+
+
+
+]
